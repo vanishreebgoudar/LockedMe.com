7cf75fcaa1d82205d70d165f67cf7ca2260e977e
unknown <biradargoudarvani@gmail.com>
2022-10-18 13:36:05 +0530

    second commit for 1st assignemt projet

--- MyFileSystem.java
+++ MyFileSystem.java
@@ -8,12 +8,14 @@ public class MyFileSystem
 
 	public static void main(String[] args) throws IOException
 	{
-		int choiceMain;
-		char choiceSub;
+		int choiceMain,choiceSub;
+		
 		Scanner sc=new Scanner(System.in);
+		String fileName;	
+        File f;
 		
 		System.out.println("********: Welcome to File Management Application :*********");
-		System.out.println("_________ Developed By : Vanishree M B__________\n");
+		System.out.println("_________ Developed By : Vanishree M Biradargoudar__________\n");
 		System.out.println("Select Your Option");
 		Mainmenu:
 		    do {
@@ -22,43 +24,47 @@ public class MyFileSystem
 		         System.out.println("2.To perform add or delete or search the file");
 		         System.out.println("3.Exit from the Application");
 	             choiceMain=sc.nextInt();
+	             
 	            switch(choiceMain)
 	            {
 	             case 1:System.out.println("List of File names in current directory");
 	                    String[] pathnames;
-	                    File f = new File("D:/OfficeFiles");
+	                     f = new File("C:/Users/Lenovo/eclipse-workspace/AssignmentProj1");
 	                    pathnames = f.list();
                         for (String pathname : pathnames) 
                        {
                            System.out.println("\t"+pathname);
 	                   }
                        break;
-	             case 2:System.out.println("\t 1.Add a file to the existing directory list");
-	                    System.out.println("\t 2.Delete a user specified file from the existing directory list");
-		                System.out.println("\t 3.Search a user specified file from the main directory");
+	             case 2:System.out.println("\t 1.To Add a file to the existing directory list");
+	                    System.out.println("\t 2.To Delete a file from the existing directory list");
+		                System.out.println("\t 3.To Search a file from the main directory");
 		                System.out.println("\t 4.Back to Main menu");  
 		                //System.out.println("\t 5.Exit ");
-		                choiceSub=sc.next().charAt(0);
-		                String fileName;	
+		                choiceSub=sc.nextInt();
+		                
 		            	   switch(choiceSub)
 			               {
-			                    case 1: System.out.println("\tEnter the file Name to Create:");
+			                    case 1: System.out.println("\tEnter the file name to create:");
 			                             fileName=sc.next();                  
-			                            File f1=new File(fileName);
-	                                    if(f1.createNewFile()) System.out.println("File is created");
-	                                    else System.out.println("File already exists");
-	                                    System.out.println(f1.getAbsolutePath());
-	                                    break;
+			                             f =new File(fileName);
+	                                     if(f.createNewFile()) System.out.println("File is created");
+	                                     else System.out.println("File already exists");
+	                                     System.out.println(f.getAbsolutePath());
+	                             break;
 		   
-			                    case 2:System.out.println("\tEnter the file Name to delete :");
+			                    case 2:System.out.println("\tEnter the file name to delete :");
 	                                   fileName=sc.next();
-	                                   File f3= new File(fileName);            
-			        	               if(f3.delete()) System.out.println(f3.getName() + " deleted");    
+	                                    f= new File(fileName);            
+			        	               if(f.delete()) System.out.println(f.getName() + " deleted");    
 			        	               else  System.out.println("failed"); 
 			        	               break; 
-			                    case 3:System.out.println("\tEnter the file Name to Search:");
+			                    case 3:System.out.println("\tEnter the file name to Search:");
 	                                   fileName=sc.next();
-			                    	   break;
+	                                    f = new File(fileName);
+	                                   if (f.exists())System.out.println("Exists");
+	                                   else System.out.println("Does not Exists");
+	                                   break;
 			                    case 4:continue Mainmenu;
 			                   
 			                                   
@@ -69,7 +75,7 @@ public class MyFileSystem
 		       
 	      } 
 		}while(choiceMain!=3);
-		System.out.println("Thank you ");
+		System.out.println("********Thank you for using the File Mangement System Application*********");
 
 	}
 }
